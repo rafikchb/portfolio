6067a8fb789620d6bcb79f720565ab14eec9ef4f
rafikchb <rafikchb@hotmail.fr>
2021-10-08 01:34:14 +0200

    adding the X icon

--- hello/hello.js
+++ hello/hello.js
@@ -1,10 +1,17 @@
 let burger = document.getElementById('burger');
-let navMenu = document.getElementById('navMenu'); 
-burger.addEventListener('click',()=>{ 
-    if(navMenu.classList.contains('hidden')){
-        navMenu.classList.remove('hidden') ;  
+let navMenu = document.getElementById('navMenu');
+burger.addEventListener('click', () => {
+    if (navMenu.classList.contains('hidden')) {
+        navMenu.classList.remove('hidden');
+        burger.classList.remove('open');
+        burger.classList.add('close');
 
-    }else{ 
-navMenu.classList.add('hidden'); 
+
+
+    } else {
+        navMenu.classList.add('hidden');
+
+        burger.classList.remove('close');
+        burger.classList.add('open');
     }
-});  
+});
